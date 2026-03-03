7d8ef46fdca5c360e289d66b6a57d7f64c75254e
Denis Yunyaev <music_is_my_extazy@mail.ru>
2026-03-04 01:26:33 +0300

    fix4

--- prisma.config.ts
+++ prisma.config.ts
@@ -7,7 +7,6 @@ export default defineConfig({
   schema: 'prisma/schema.prisma',
   migrations: {
     path: 'prisma/migrations',
-    seed: 'npx tsx ./prisma/seed.ts',
   },
   datasource: {
     // В Prisma 7 это поле заменяет и url, и directUrl для миграций
